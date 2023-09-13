a88d5528bf063f2903c33281ff5537ae44989eb0
james-o <jaydhales@gmail.com>
2023-09-13 04:19:59 +0100

    voting contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,13 +1,15 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
 contract Voting {
-    struct Election {
+    struct Candidates {
+        address id;
         uint votes;
+    }
+    struct Election {
         string position;
+        Candidates[] candidates;
+        bool isActive;
     }
 
     enum Position {
@@ -16,12 +18,16 @@ contract Voting {
         Senate
     }
 
-    address public electoralAdmin;
+    address electoralAdmin;
 
-    mapping(address => Election) public candidates;
+    mapping(Position => Election) public elections;
+    mapping(address => mapping(Position => bool)) hasVoted;
     mapping(Position => mapping(address => bool)) public isCandidate;
-    mapping(address => mapping(Position => bool)) public hasVoted;
-    mapping(Position => bool) hasCreated;
+    mapping(address => mapping(Position => uint)) candidateIndex;
+
+    constructor() {
+        electoralAdmin = msg.sender;
+    }
 
     modifier onlyAdmin() {
         require(
@@ -35,24 +41,47 @@ contract Voting {
         Position _position,
         address[] memory _candidates
     ) external onlyAdmin {
-        require(!hasCreated[_position], "Election is still running");
+        Election storage newElection = elections[_position];
+        require(
+            !newElection.isActive && newElection.candidates.length == 0,
+            "Election has been Created"
+        );
+
+        newElection.isActive = true;
+        newElection.position = _addPosition(_position);
         for (uint i = 0; i < _candidates.length; i++) {
             address _candidate = _candidates[i];
-            Election storage newElection = candidates[_candidate];
-
-            newElection.position = _addPosition(_position);
+            Candidates memory _cand;
+            _cand.id = _candidate;
+            newElection.candidates.push(_cand);
             isCandidate[_position][_candidate] = true;
+            candidateIndex[_candidate][_position] = i;
         }
     }
 
     function vote(Position _position, address _candidate) external {
-        require(
-            isCandidate[_position][_candidate],
-            "Address is not a candidate"
-        );
-        require(!hasVoted[msg.sender][_position], "Already Voted");
-        Election storage elect = candidates[_candidate];
-        elect.votes++;
+        Election storage _election = elections[_position];
+        require(_election.isActive, "Election is not active");
+        require(isCandidate[_position][_candidate], "Candidate does not exist");
+        require(!hasVoted[msg.sender][_position], "User has Voted");
+
+        uint _index = candidateIndex[_candidate][_position];
+        _election.candidates[_index].votes++;
+        hasVoted[msg.sender][_position] = true;
+    }
+
+    function closeElection(Position _position) external onlyAdmin {
+        Election storage _election = elections[_position];
+        _election.isActive = false;
+    }
+
+    function viewResult(
+        Position _position
+    ) external view returns (string memory, Candidates[] memory) {
+        Election memory _election = elections[_position];
+        string memory title = _election.position;
+        Candidates[] memory _candidates = _election.candidates;
+        return (title, _candidates);
     }
 
     function _addPosition(
